0b7bf4f6d17406a21134b3f5aedf2a37b7450cdc
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-07-13 13:45:38 +0900

    [Feat] router.tsx 설정

---
+++ src/router.tsx
@@ -0,0 +1,31 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+
+import DetailPage from './pages/detail/DetailPage';
+import Kakaomap from './pages/map/Kakaomap';
+import LoginPage from './pages/login/LoginPage';
+import MainPage from './pages/main/MainPage';
+import MylistPage from './pages/mylist/MylistPage';
+import NotfoundPage from './pages/notfound/NotfoundPage';
+import ReviewPage from './pages/review/ReviewPage';
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      {/* 지도 바탕을 사용하는 페이지들 */}
+      <Route path='/' element={<Kakaomap />}>
+        <Route index element={<MainPage />} />
+        <Route path='/detail' element={<DetailPage />} />
+        <Route path='/review' element={<ReviewPage />} />
+        <Route path='/mylist' element={<MylistPage />} />
+        <Route path='*' element={<NotfoundPage />} />
+      </Route>
+
+      {/* 지도 바탕이 필요없는 페이지들*/}
+      <Route path='/login' element={<LoginPage />} />
+    </>
+  )
+);
