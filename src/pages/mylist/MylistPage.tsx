d142d4f42fada978236a4ab7ac8c462cb59d3a15
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-07-16 13:01:25 +0900

    [Feat] 페이지 선택 여부에 따라 서브페이지 연결

--- src/pages/mylist/MylistPage.tsx
+++ src/pages/mylist/MylistPage.tsx
@@ -1,11 +1,19 @@
+import Header from '../../components/mylist/Header';
+import MyHistory from '../../components/mylist/MyHistory';
+import MyRanking from './MyRanking';
 import styles from './mylist.module.css';
+import { useState } from 'react';
 
 function MylistPage() {
-    return (
-        <div className={styles.container}>
-            <div>This is MyList Page</div>
-        </div>
-    );
+  const [selectedTab, setSelectedTab] = useState<'history' | 'ranking'>(
+    'history'
+  );
+  return (
+    <div className={styles.container}>
+      <Header selectedTab={selectedTab} onChange={setSelectedTab} />
+      {selectedTab === 'history' ? <MyHistory /> : <MyRanking />}
+    </div>
+  );
 }
 
 export default MylistPage;
