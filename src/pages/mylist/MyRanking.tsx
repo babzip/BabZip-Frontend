e5244f323bee25cd90ec2db099c8f61f25c4509c
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-07-16 13:01:05 +0900

    [Feat] 랭킹 서브페이지 초기 구성

---
+++ src/pages/mylist/MyRanking.tsx
@@ -0,0 +1,11 @@
+import styles from './myranking.module.css';
+
+const MyRanking = () => {
+  return (
+    <div className={styles.container}>
+      <div>MyRanking 컴포넌트</div>
+    </div>
+  );
+};
+
+export default MyRanking;
