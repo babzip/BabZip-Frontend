abde5cb2e340cda29d3c6321d219e94f9692b302
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-07-16 13:00:52 +0900

    [Feat] 내 기록 서브페이지 초기구성

---
+++ src/components/mylist/MyHistory.tsx
@@ -0,0 +1,11 @@
+import styles from './myhistory.module.css';
+
+const MyHistory = () => {
+  return (
+    <div className={styles.container}>
+      <div>MyHistory 컴포넌트</div>
+    </div>
+  );
+};
+
+export default MyHistory;
