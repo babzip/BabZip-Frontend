ddf3d8cdd81d36d5c2af05ec6f34f4a427d6247d
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2025-07-17 15:42:07 +0900

    [Feat] 추가하기 페이지 초기구성

---
+++ src/pages/addlist/AddListPage.tsx
@@ -0,0 +1,11 @@
+import styles from './addListPage.module.css';
+
+function AddListPage() {
+  return (
+    <div className={styles.container}>
+      <div>AddListPage</div>
+    </div>
+  );
+}
+
+export default AddListPage;
--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,7 @@ import {
   createRoutesFromElements,
 } from 'react-router-dom';
 
+import AddListPage from './pages/addlist/AddListPage';
 import DetailPage from './pages/detail/DetailPage';
 import Kakaomap from './pages/map/Kakaomap';
 import LoginPage from './pages/login/LoginPage';
@@ -25,6 +26,7 @@ export const router = createBrowserRouter(
 
       {/* 지도 바탕이 필요없는 페이지들*/}
       <Route path='/mylist' element={<MylistPage />} />
+      <Route path='/mylist/addlist' element={<AddListPage />} />
       <Route path='/login' element={<LoginPage />} />
     </>
   )
